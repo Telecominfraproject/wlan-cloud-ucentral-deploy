3d94d6849cb2ed748a15412fba139552ce855d2c
oblom0v <johann.hoffmann@mailbox.org>
2021-09-07 23:32:57 +0200

    Add Compose file with acme.sh service and split deployments
    Rename env-files

---
+++ docker-compose/acme.sh/custom_entry.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+UCENTRALGW_HOSTNAMES="$UCENTRALGW_HOSTNAME $INTERNAL_UCENTRALGW_HOSTNAME"
+UCENTRALSEC_HOSTNAMES="$UCENTRALSEC_HOSTNAME $INTERNAL_UCENTRALSEC_HOSTNAME"
+UCENTRALFMS_HOSTNAMES="$UCENTRALFMS_HOSTNAME $INTERNAL_UCENTRALFMS_HOSTNAME"
+
+if [ ! -d /acme.sh/ca ]; then
+    echo "First deployment, issuing certificates."
+    acme.sh --set-default-ca --server letsencrypt
+
+    for hostname in $UCENTRALGW_HOSTNAMES; do
+        acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralgw_certs --cert-file /acme.sh/ucentralgw_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralgw_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralgw_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
+    done
+
+    for hostname in $UCENTRALSEC_HOSTNAMES; do
+        acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralsec_certs --cert-file /acme.sh/ucentralsec_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralsec_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralsec_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
+    done
+
+    for hostname in $UCENTRALFMS_HOSTNAMES; do
+        acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralfms_certs --cert-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
+    done
+fi
+
+echo "Switching into daemon mode."
+exec /entry.sh daemon
