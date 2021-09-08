5cc0977659b2b25d2d95f4ad200e39a05d0cebf2
oblom0v <johann.hoffmann@mailbox.org>
2021-09-08 12:23:40 +0200

    Add Letsencrypt certs for rttys

--- docker-compose/acme.sh/custom_entry.sh
+++ docker-compose/acme.sh/custom_entry.sh
@@ -19,6 +19,8 @@ if [ ! -d /acme.sh/ca ]; then
     for hostname in $UCENTRALFMS_HOSTNAMES; do
         acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralfms_certs --cert-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
     done
+
+    acme.sh --issue -d "$RTTYS_HOSTNAME" --cert-home /acme.sh/rttys_certs --cert-file /acme.sh/rttys_certs/restapi-cert.pem --key-file /acme.sh/rttys_certs/restapi-key.pem --ca-file /acme.sh/rttys_certs/restapi-ca.pem -w /acme.sh/letsencrypt
 fi
 
 echo "Switching into daemon mode."
