b9a5090e9a9b33f50fe9e0a0114da6b0edd89523
oblom0v <johann.hoffmann@mailbox.org>
2021-09-08 14:04:42 +0200

    Remove public and internal hostname separation since microservices will communicate over internal Docker Compose network anyway

--- docker-compose/acme.sh/custom_entry.sh
+++ docker-compose/acme.sh/custom_entry.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
-UCENTRALGW_HOSTNAMES="$UCENTRALGW_HOSTNAME $INTERNAL_UCENTRALGW_HOSTNAME"
-UCENTRALSEC_HOSTNAMES="$UCENTRALSEC_HOSTNAME $INTERNAL_UCENTRALSEC_HOSTNAME"
-UCENTRALFMS_HOSTNAMES="$UCENTRALFMS_HOSTNAME $INTERNAL_UCENTRALFMS_HOSTNAME"
-
 if [ ! -d /acme.sh/ca ]; then
     echo "First deployment, issuing certificates."
     acme.sh --set-default-ca --server letsencrypt
 
-    for hostname in $UCENTRALGW_HOSTNAMES; do
-        acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralgw_certs --cert-file /acme.sh/ucentralgw_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralgw_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralgw_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
-    done
+    acme.sh --issue -d "$UCENTRALGW_HOSTNAME" --cert-home /acme.sh/ucentralgw_certs --cert-file /acme.sh/ucentralgw_certs/restapi-cert.pem --key-file /acme.sh/ucentralgw_certs/restapi-key.pem --ca-file /acme.sh/ucentralgw_certs/restapi-ca.pem -w /acme.sh/letsencrypt
 
-    for hostname in $UCENTRALSEC_HOSTNAMES; do
-        acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralsec_certs --cert-file /acme.sh/ucentralsec_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralsec_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralsec_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
-    done
+    acme.sh --issue -d "$UCENTRALSEC_HOSTNAME" --cert-home /acme.sh/ucentralsec_certs --cert-file /acme.sh/ucentralsec_certs/restapi-cert.pem --key-file /acme.sh/ucentralsec_certs/restapi-key.pem --ca-file /acme.sh/ucentralsec_certs/restapi-ca.pem -w /acme.sh/letsencrypt
 
-    for hostname in $UCENTRALFMS_HOSTNAMES; do
-        acme.sh --issue -d "$hostname" --cert-home /acme.sh/ucentralfms_certs --cert-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-cert.pem --key-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-key.pem --ca-file /acme.sh/ucentralfms_certs/"$hostname"/restapi-ca.pem -w /acme.sh/letsencrypt
-    done
+    acme.sh --issue -d "$UCENTRALFMS_HOSTNAME" --cert-home /acme.sh/ucentralfms_certs --cert-file /acme.sh/ucentralfms_certs/restapi-cert.pem --key-file /acme.sh/ucentralfms_certs/restapi-key.pem --ca-file /acme.sh/ucentralfms_certs/restapi-ca.pem -w /acme.sh/letsencrypt
 
     acme.sh --issue -d "$RTTYS_HOSTNAME" --cert-home /acme.sh/rttys_certs --cert-file /acme.sh/rttys_certs/restapi-cert.pem --key-file /acme.sh/rttys_certs/restapi-key.pem --ca-file /acme.sh/rttys_certs/restapi-ca.pem -w /acme.sh/letsencrypt
 fi
