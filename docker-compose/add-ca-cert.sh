bf2be75f81fff65f27afa39797423dfd54cbac79
oblom0v <johann.hoffmann@mailbox.org>
2021-09-09 18:45:23 +0200

    Adapt script to deployment split

--- docker-compose/add-ca-cert.sh
+++ docker-compose/add-ca-cert.sh
@@ -4,7 +4,7 @@ set -e
 SERVICES="ucentralgw ucentralsec ucentralfms"
 
 for i in $SERVICES; do
-    docker-compose exec -T $i apk add ca-certificates
+    docker-compose -f docker-compose.yml -f docker-compose.selfsigned.yml --env-file .env.selfsigned exec -T $i apk add ca-certificates
     docker cp certs/restapi-ca.pem ucentral_$i\_1:/usr/local/share/ca-certificates/
-    docker-compose exec -T $i update-ca-certificates
+    docker-compose -f docker-compose.yml -f docker-compose.selfsigned.yml --env-file .env.selfsigned exec -T $i update-ca-certificates
 done
