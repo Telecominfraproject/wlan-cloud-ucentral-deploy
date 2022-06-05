e058e995a07922f7ddd50885b76e0bea1cd8ff20
Johann Hoffmann <johann.hoffmann@mailbox.org>
2022-06-05 22:38:00 +0200

    Expose prov and prov-ui ports in Letsencrypt deployment after switch to one hostname
    Signed-off-by: Johann Hoffmann <johann.hoffmann@mailbox.org>

--- docker-compose/deploy.sh
+++ docker-compose/deploy.sh
@@ -177,4 +177,8 @@ if [[ ! -z "$TRAEFIK_ACME_EMAIL" ]]; then
 fi
 
 # Run the deployment
-docker-compose -f docker-compose.lb.letsencrypt.yml --env-file .env.letsencrypt up -d
+if [[ ! -z "$SDKHOSTNAME" ]]; then
+  docker-compose -f docker-compose.lb.letsencrypt.yml --env-file .env.letsencrypt up -d
+else
+  docker-compose up -d
+fi
