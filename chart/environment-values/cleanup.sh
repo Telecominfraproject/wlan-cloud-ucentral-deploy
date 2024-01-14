5aa25cf324d4ee992a4e2b5cb61e231fa4ccad1e
stephb9959 <stephane.bourque@gmail.com>
2024-01-14 13:23:09 -0800

    https://telecominfraproject.atlassian.net/browse/WIFI-13280
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- chart/environment-values/cleanup.sh
+++ chart/environment-values/cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 [ -z "$NAMESPACE" ] && echo "No NAMESPACE set" && exit 1
+echo "Deleting openwifi-$NAMESPACE"
 helm -n openwifi-$NAMESPACE delete tip-openwifi
-sleep 30
+sleep 15
+echo "Deleting namespace openwifi-$NAMESPACE"
 kubectl delete ns openwifi-$NAMESPACE
+exit 0
