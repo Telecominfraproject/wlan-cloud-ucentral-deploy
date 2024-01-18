fa967934c92bf6d62f7b2bae19dddbe8e917816a
stephb9959 <stephane.bourque@gmail.com>
2024-01-17 22:33:35 -0800

    https://telecominfraproject.atlassian.net/browse/WIFI-13280
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- chart/environment-values/cleanup.sh
+++ chart/environment-values/cleanup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 [ -z "$NAMESPACE" ] && echo "No NAMESPACE set" && exit 1
-echo "Deleting openwifi-$NAMESPACE"
-helm -n openwifi-"$NAMESPACE" delete tip-openwifi
-sleep 15
-echo "Deleting namespace openwifi-$NAMESPACE"
-kubectl delete ns openwifi-"$NAMESPACE"
-# leave this exit 0 here, because if the namespace does not exist, the script should not fail
-exit 0
+helm -n openwifi-$NAMESPACE delete tip-openwifi
+sleep 30
+kubectl delete ns openwifi-$NAMESPACE
