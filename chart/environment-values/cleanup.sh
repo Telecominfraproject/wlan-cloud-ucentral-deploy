a377966ac97d4ce958641c869b44c1752cdefea6
Carsten Schafer <Carsten.Schafer@kinarasystems.com>
2024-04-15 11:34:46 -0400

    initial Kafka version changes
    Signed-off-by: Carsten Schafer <Carsten.Schafer@kinarasystems.com>

--- chart/environment-values/cleanup.sh
+++ chart/environment-values/cleanup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 [ -z "$NAMESPACE" ] && echo "No NAMESPACE set" && exit 1
-helm -n openwifi-"$NAMESPACE" delete tip-openwifi
+ns="openwifi-$NAMESPACE"
+echo "Cleaning up namespace $ns in 10 seconds..."
+sleep 10
+echo "- delete tip-openwifi helm release in $ns"
+helm -n "$ns" delete tip-openwifi
 if [[ "$1" == "full" ]] ; then
+    echo "- delete $ns namespace in 30 seconds..."
     sleep 30
-    kubectl delete ns openwifi-"$NAMESPACE"
+    echo "- delete $ns namespace"
+    kubectl delete ns "$ns"
 fi
+echo "- cleaned up $ns namespace"
 exit 0
