dceb24d0bba36d337167b258d4ad71d9e9c7ecbc
Carsten Schafer <Carsten.Schafer@kinarasystems.com>
2024-07-26 16:42:33 -0400

    SQL init script can't work, DB check to last one
    Signed-off-by: Carsten Schafer <Carsten.Schafer@kinarasystems.com>

--- docker-compose/postgresql/init-db.sh
+++ docker-compose/postgresql/init-db.sh
@@ -14,6 +14,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE $OWANALYTICS_DB OWNER $OWANALYTICS_DB_USER;
     CREATE USER $OWSUB_DB_USER WITH ENCRYPTED PASSWORD '$OWSUB_DB_PASSWORD';
     CREATE DATABASE $OWSUB_DB OWNER $OWSUB_DB_USER;
-    \c owsec
-    INSERT INTO users (id, name, description, avatar, email, validated, validationemail, validationdate, creationdate, validationuri, changepassword, lastlogin, currentloginuri, lastpasswordchange, lastemailcheck, waitingforemailcheck, locale, notes, location, owner, suspended, blacklisted, userrole, usertypeproprietaryinfo, securitypolicy, securitypolicychange, currentpassword, lastpasswords, oauthtype, oauthuserinfo, modified, signingup) VALUES ('11111111-0000-0000-6666-999999999999', 'Default User', 'Default user should be deleted.', '1701287135', 'tip@ucentral.com', true, '', 0, 1683468668, '', true, 1702156150, '', 1683468881, 0, false, '', '[]', '', '', false, false, 'root', '{"authenticatorSecret":"","mfa":{"enabled":false,"method":""},"mobiles":[]}', '', 0, '13268b7daa751240369d125e79c873bd8dd3bef7981bdfd38ea03dbb1fbe7dcf', '["13268b7daa751240369d125e79c873bd8dd3bef7981bdfd38ea03dbb1fbe7dcf","1683468881040646067|bcf145fa6354dbe1baddf1f66f6acc2466c49c2fbc22d2150a7f7a94fa1cfa27"]', '', '', '1683468881', '') ON CONFLICT (id) DO NOTHING;
 EOSQL
